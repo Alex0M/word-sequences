bc0ea0f6b036ff6ceac883b32a95565e0f2cdad0
Alex0M <alex.sudom@gmail.com>
2023-01-23 16:19:16 -0600

    Added comments to main.go file

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 )
 
+// dictionary to sort and print info about word sequences and their numbers
 type WordSequencesCount struct {
 	words string
 	count int
@@ -27,6 +28,7 @@ func splitTextToWords(t string) []string {
 	return words
 }
 
+// Function to count word sequences and add them to map
 func countWordSequences(sWords []string, swfpl []string, wsn int) []string {
 	s := append(swfpl, sWords...)
 
@@ -43,6 +45,7 @@ func countWordSequences(sWords []string, swfpl []string, wsn int) []string {
 	return swfpl
 }
 
+// Function to read a file in chunks of some size, process them, and repeat until the end. We don't want to load a large file into memory.
 func readFromFile(file *os.File, bufferSize int, wsn int) {
 	var swfpl []string
 	buffer := make([]byte, bufferSize)
@@ -59,6 +62,7 @@ func readFromFile(file *os.File, bufferSize int, wsn int) {
 			break
 		}
 
+		//make sure that the end of chunk is a space or '\n' if not then we need to read only until last space or last \n
 		readBytes = b
 		for i := b - 1; i > 1; i-- {
 			if string(buffer[i]) == "\n" || string(buffer[i]) == " " {
@@ -67,17 +71,18 @@ func readFromFile(file *os.File, bufferSize int, wsn int) {
 			}
 		}
 
-		sWords := splitTextToWords(string(buffer[:readBytes]))
-		swfpl = countWordSequences(sWords, swfpl, wsn)
+		sWords := splitTextToWords(string(buffer[:readBytes])) //split chunk to words
+		swfpl = countWordSequences(sWords, swfpl, wsn)         //count word sequences
 
-		totalReadBytes = totalReadBytes + int64(readBytes)
-		_, errSeek := file.Seek(totalReadBytes, 0)
+		totalReadBytes = totalReadBytes + int64(readBytes) //calculate how many bytes we already read in file
+		_, errSeek := file.Seek(totalReadBytes, 0)         //set the offset for next reading
 		if errSeek != nil {
 			log.Panicln(err)
 		}
 	}
 }
 
+// Function to read text from os.Stdin
 func readFromStdin(file *os.File, wsn int) {
 	var swfpl []string
 
@@ -101,9 +106,9 @@ func readFromStdin(file *os.File, wsn int) {
 
 func main() {
 	wsc := make([]WordSequencesCount, 0, len(m))
-	bufferSize := 1 * 1024 * 1024
-	wsn := 3
-	mcn := 100
+	bufferSize := 1 * 1024 * 1024 //a buffer size for fole reading. For example 1MB
+	wsn := 3                      //number of word sequences that need to be calculated. We can calucate 1,2,3,4,5 etc. word sequences
+	mcn := 100                    //number of the most common word sequences to output.
 
 	if len(os.Args[1:]) == 0 {
 		readFromStdin(os.Stdin, wsn)
@@ -120,14 +125,17 @@ func main() {
 		}
 	}
 
+	//add info from map to dict
 	for k, v := range m {
 		wsc = append(wsc, WordSequencesCount{words: k, count: v})
 	}
 
+	//sort dict
 	sort.Slice(wsc, func(i, j int) bool {
 		return wsc[i].count > wsc[j].count
 	})
 
+	//print
 	for i := 0; i < len(wsc) && i < mcn; i++ {
 		fmt.Println(wsc[i].words, "-", wsc[i].count)
 	}
